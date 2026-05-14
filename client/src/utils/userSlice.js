048aa176709aa847f289294b17dcab82323589c8
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-05-15 02:49:41 +0530

    feat: add initial redux store code

---
+++ client/src/utils/userSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: null,
+  reducers: {
+    addUser: (state, action) => {
+      return action.payload;
+    },
+    removeUser: (state, action) => {
+      return null;
+    },
+  },
+});
+export const { addUser, removeUSer } = userSlice.action;
+export default userSlice.reducer;
