e38c870106f32e5d1a6170a5a2c7f3f8e8014bd6
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-05-23 15:27:49 +0530

    feat: add dynamic behavior to InviteAndStatus

--- client/src/Components/InviteAndStatus.jsx
+++ client/src/Components/InviteAndStatus.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const InviteAndStatus = () => {
+  const user = useSelector((data) => data.user);
+
+  const avatarMap = {
+    "001": "/avatarM01.png",
+    "002": "/avatarM02.png",
+    "003": "/avatarF03.png",
+    "004": "/avatarF04.png",
+  };
+
   const [members, setMembers] = useState([
-    { name: "Yuvraj", status: "online" },
+    { name: user.username, status: "online", avatar: user.avatar },
   ]);
   return (
     <div className=" flex flex-col pt-10 p-5">
@@ -10,10 +20,16 @@ const InviteAndStatus = () => {
         PEOPLE (<span className="text-sm">{members.length}</span>)
       </h1>
 
-      {members.map(({ name, status }, i) => {
+      {members.map(({ name, status, avatar }, i) => {
         return (
-          <div key={i} className="flex  mt-5 gap-2  items-center">
-            <div className=" rounded-full shrink-0 border w-10 h-10"></div>
+          <div key={i} className="flex   mt-5 gap-2  items-center">
+            <div className=" rounded-full shrink-0 border w-10 h-10">
+              <img
+                src={avatarMap[avatar]}
+                alt="avatar"
+                className="w-full h-full rounded-full"
+              />
+            </div>
             <div className="flex  flex-1 flex-col min-w-0 ">
               <div className="capitalize  break-all  ">{name}</div>
               <div className=" flex  items-center gap-1">
